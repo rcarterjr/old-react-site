f4f04fd2bb1f4568fa6d1b906f95cb01a01db7c4
Russell Carter <russell.c12@gmail.com>
2018-09-15 20:03:35 -0400

    My portfolio

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Particles from 'react-particles-js';
+import ContentComponent from './App2.js';
+import AboutMeComponent from './App3.js';
+
+
+class ParticleComponent extends Component {
+  render() {
+    return (
+        <div style={{height: '100vh'}}>
+        <Particles
+            height="100vh"
+            params={{
+                "particles": {
+                    "number": {
+                        "value": 55
+                    },
+                    "size": {
+                        "value": 3
+                    }
+                },
+                "interactivity": {
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "repulse"
+                        }
+                    }
+                }
+            }} />
+            <ContentComponent/>
+            <AboutMeComponent/>
+        </div>
+    );
+
+
+  }
+}
+
+export default ParticleComponent;
