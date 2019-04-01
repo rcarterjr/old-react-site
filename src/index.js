c63ceae2a0465a0e9ea1f86374f368030948fe4e
Russell Carter <russell.c12@gmail.com>
2019-04-01 18:44:17 -0400

    GHP

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ParticleComponent from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap';
-
-ReactDOM.render(<ParticleComponent />, document.getElementById('root'));
-registerServiceWorker();
